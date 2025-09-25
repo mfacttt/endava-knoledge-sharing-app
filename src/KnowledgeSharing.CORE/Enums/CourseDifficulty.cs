545a0b83f71a09368c4dd350eacb9b0ad1fbf640
Alex Pekel <Alex.Pekel@endava.com>
2025-09-25 17:20:39 +0300

    Add enums for course difficulty and disciplines

---
+++ src/KnowledgeSharing.CORE/Enums/CourseDifficulty.cs
@@ -0,0 +1,11 @@
+namespace KnowledgeSharing.CORE.Enums;
+
+/// <summary>
+/// Difficulty of the course._
+/// </summary>
+public enum CourseDifficulty
+{
+    Introductory = 1,
+    Medium = 2,
+    Advanced = 3
+}
---
+++ src/KnowledgeSharing.CORE/Enums/CourseDisciplines.cs
@@ -0,0 +1,13 @@
+namespace KnowledgeSharing.CORE.Enums;
+
+/// <summary>
+/// Discipline of the course._
+/// </summary>
+public enum CourseDisciplines
+{
+    Development = 1,
+    DevOps = 2,
+    Testing = 3,
+    ApplicationManagement = 4,
+    ProjectManagement = 5
+}
