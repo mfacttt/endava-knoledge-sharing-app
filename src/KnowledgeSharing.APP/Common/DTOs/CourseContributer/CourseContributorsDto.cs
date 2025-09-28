2a94a3923331911d9d7c6ebee5728be0c51c7e61
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 01:50:06 +0300

    Initialize colection to avoid nulls

--- src/KnowledgeSharing.APP/Common/DTOs/CourseContributer/CourseContributorsDto.cs
+++ src/KnowledgeSharing.APP/Common/DTOs/CourseContributer/CourseContributorsDto.cs
@@ -2,5 +2,5 @@ namespace KnowledgeSharing.APP.Common.DTOs.CourseContributor;
 
 public sealed class CourseContributorsDto
 {
-    public IEnumerable<CourseContributorDto> Contributors { get; init; } = default!;
+    public IEnumerable<CourseContributorDto> Contributors { get; init; } = Array.Empty<CourseContributorDto>();
 }
--- src/KnowledgeSharing.APP/Common/DTOs/CourseEnrollment/CourseEnrollmentsDto.cs
+++ src/KnowledgeSharing.APP/Common/DTOs/CourseEnrollment/CourseEnrollmentsDto.cs
@@ -2,5 +2,5 @@ namespace KnowledgeSharing.APP.Common.DTOs.CourseEnrollment;
 
 public sealed class CourseEnrollmentsDto
 {
-    public IEnumerable<CourseEnrollmentDto> Enrollments { get; init; } = default!;
+    public IEnumerable<CourseEnrollmentDto> Enrollments { get; init; } = Array.Empty<CourseEnrollmentDto>();
 }
