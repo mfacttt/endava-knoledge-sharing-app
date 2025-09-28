39bead73ff7e1800d1670eb4f5e8ea06357c834d
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 01:52:55 +0300

    Update course mapping profile

--- src/KnowledgeSharing.APP/Common/Mappings/CourseMappingProfile.cs
+++ src/KnowledgeSharing.APP/Common/Mappings/CourseMappingProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
-using KnowledgeSharing.APP.Features.Course.Commands.Create;
 using KnowledgeSharing.CORE.Entities;
+using KnowledgeSharing.APP.Common.DTOs;
+using KnowledgeSharing.APP.Features.Course.Commands.Create;
+using KnowledgeSharing.APP.Features.Course.Commands.Update;
 
 namespace KnowledgeSharing.APP.Common.Mappings;
 
@@ -12,7 +14,27 @@ public sealed class CourseMappingProfile : Profile
             .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
             .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
             .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.CourseDiscipline))
-            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.CourseDifficulty));   
+            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.CourseDifficulty));
 
+        CreateMap<UpdateCourseCommand, Course>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.CourseDiscipline))
+            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.CourseDifficulty));
+
+        // Exept Created - it will be mapped in the handler
+        CreateMap<Course, CourseInfoDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Discipline, opt => opt.MapFrom(src => src.Discipline))
+            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.Difficulty))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+            .ForMember(d => d.Modules, opt => opt.MapFrom(
+                src => (src.Modules ?? Array.Empty<Module>())
+                        .OrderBy(m => m.Order)
+            ));
     }
 }
