ed856c028ed58a53e9105275593a256c04856275
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 03:22:10 +0300

    Create contributor controller

---
+++ src/KnowledgeSharing.API/Controllers/ContributorController.cs
@@ -0,0 +1,35 @@
+using KnowledgeSharing.APP.Features.CourseEnrollment.Command.Enroll;
+using KnowledgeSharing.APP.Features.CourseEnrollment.Command.Unenroll;
+using KnowledgeSharing.APP.Features.CourseEnrollment.Queries.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+public class ContributorController(ISender sender) : ControllerBase
+{
+    [HttpPost("{courseId:int}/[controller]")]
+    public async Task<IActionResult> Add([FromRoute] int courseId, CancellationToken cancellationToken)
+    {
+        var response = await sender.Send(new EnrollUserCommand(courseId), cancellationToken);
+        if (!response.IsSuccess)
+            return BadRequest(response.Errors);
+        return Ok();
+    }
+
+    [HttpDelete("{courseId:int}/[controller]")]
+    public async Task<IActionResult> Remove([FromRoute] int courseId, CancellationToken cancellationToken)
+    {
+        var response = await sender.Send(new UnenrollUserCommand(courseId), cancellationToken);
+        if (!response.IsSuccess)
+            return BadRequest(response.Errors);
+        return Ok();
+    }
+
+    [HttpGet("{courseId:int}/[controller]")]
+    public async Task<IActionResult> Get([FromRoute] int courseId, CancellationToken cancellationToken)
+    {
+        var response = await sender.Send(new GetCourseEnrollmentsQuery(courseId), cancellationToken);
+        if (!response.IsSuccess)
+            return BadRequest(response.Errors);
+        return Ok(response.Data);
+    }
+}
