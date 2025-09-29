f437189afeed367635c5603958939ce3a7f0a576
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 13:25:20 +0300

    Add cancelation token as parament

--- src/KnowledgeSharing.CORE/Interfaces/Persistence/ICourseUserRepository.cs
+++ src/KnowledgeSharing.CORE/Interfaces/Persistence/ICourseUserRepository.cs
@@ -2,7 +2,8 @@ namespace KnowledgeSharing.CORE.Interfaces.Persistence;
 
 public interface ICourseUserRepository<TUser>
 {
-    Task AddUser(int courseId, Guid userId);
-    Task RemoveUser(int courseId, Guid userId);
-    Task<IEnumerable<TUser>> GetAll(int courseId);
+    Task<bool> EnrollUser(TUser user, CancellationToken cancellationToken);
+    Task<bool> UnenrollUser(TUser user, CancellationToken cancellationToken);
+    Task<bool> IsExisting(TUser user, CancellationToken cancellationToken);
+    Task<IEnumerable<TUser>> GetAll(int courseId, CancellationToken cancellationToken);
 }
