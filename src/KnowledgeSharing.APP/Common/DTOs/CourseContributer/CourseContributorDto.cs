52c084a6d2a36d0477df9f803abe5bdc1a9a2a21
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 17:32:10 +0300

    Update dto

--- src/KnowledgeSharing.APP/Common/DTOs/CourseContributer/CourseContributorDto.cs
+++ src/KnowledgeSharing.APP/Common/DTOs/CourseContributer/CourseContributorDto.cs
@@ -3,5 +3,6 @@ namespace KnowledgeSharing.APP.Common.DTOs.CourseContributor;
 public sealed class CourseContributorDto
 {
     public Guid UserId { get; init; }
+    public int CourseId { get; init; }
     public string Name { get; init; } = "Alex";
 }
