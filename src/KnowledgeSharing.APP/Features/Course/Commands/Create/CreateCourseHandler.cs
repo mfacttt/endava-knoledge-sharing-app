1e978c77e327350dcaed4550ebd22309e2ca7af2
Alex Pekel <Alexandr.Pekel@endava.com>
2025-09-29 17:59:58 +0300

    Update create course  handler

--- src/KnowledgeSharing.APP/Features/Course/Commands/Create/CreateCourseHandler.cs
+++ src/KnowledgeSharing.APP/Features/Course/Commands/Create/CreateCourseHandler.cs
@@ -7,24 +7,37 @@ using CourseEntity = KnowledgeSharing.CORE.Entities.Course;
 
 namespace KnowledgeSharing.APP.Features.Course.Commands.Create;
 
-public sealed class CreateCourseHandler(ICourseRepository courseRepository,
-                                        IValidator<CreateCourseCommand> validator,
-                                        IMapper mapper)
+public sealed class CreateCourseHandler(IUnitOfWork unitOfWork,IMapper mapper, IValidator<CreateCourseCommand> validator)
                                         : IRequestHandler<CreateCourseCommand, Response<int>>
 {
     public async Task<Response<int>> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
     {
+        // get is from token
+        var guid = new Guid("00000000-0000-0000-0000-000000000001");
+
+        // validate request
         var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
+        // return errors if validation fails
         if (!validationResult.IsValid)
         {
             var errors = validationResult.Errors.Select(e => new ValidationErrorDto(e.PropertyName, e.ErrorMessage, e.AttemptedValue?.ToString()));
             return Response<int>.Failure(errors);
         }
 
+        // map
         var courseEf = mapper.Map<CourseEntity>(request);
+        courseEf.CreatedBy = guid;
 
-        var createdCourseId = await courseRepository.CreateAsync(courseEf, cancellationToken);
+        // create
+        var createdCourseId = await unitOfWork.Courses.CreateAsync(courseEf, cancellationToken);
+
+        // check if created
+        if (createdCourseId == 0)
+            return Response<int>.Failure(new ValidationErrorDto("Course", "Course could not be created", null));
+
+        // save changes 
+        await unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Response<int>.Success(createdCourseId);
     }
